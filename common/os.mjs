fa10990a04729ee21651225f359a34c6f85f44ee
tongtong <2713626136@qq.com>
2021-07-05 17:36:38 +0800

    调试

--- common/os.mjs
+++ common/os.mjs
@@ -1,6 +1,7 @@
 #!/usr/bin/env zx
 
 const commands = Array.from(new Set(process.argv.slice(3)))
+console.log('rrr',command)
 const defaultCommands = {
   platform: () => os.platform(),
   type: () => os.type(),
